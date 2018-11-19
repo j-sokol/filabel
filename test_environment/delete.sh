3f7561924b68f8c1eb35fa81ce1451e638387ad0
Jan Sokol <jan.sokol.glx@gmail.com>
2018-11-19 22:43:51 +0100

    remove test repo when deleting test_env

--- test_environment/delete.sh
+++ test_environment/delete.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -eu
 
+echo "HTTP DELETE https://api.github.com/repos/${GH_USER}/mi-pyt-test-repo"
+curl --header "Authorization: token ${GH_TOKEN}" -X DELETE https://api.github.com/repos/${GH_USER}/mi-pyt-test-repo
+
+
 for I in {1..4}; do
   echo "HTTP DELETE https://api.github.com/repos/${GH_USER}/filabel-testrepo${I}"
   curl --header "Authorization: token ${GH_TOKEN}" -X DELETE https://api.github.com/repos/${GH_USER}/filabel-testrepo${I}
